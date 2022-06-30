9833d0ad3d424225d1beb66bdddd1c5c2fcd07f7
Fayrral <105666460+Fayrral@users.noreply.github.com>
2022-06-30 18:17:24 -0400

    Update myfile.py
    Signed-off-by: Fayrral <105666460+Fayrral@users.noreply.github.com>

--- myfile.py
+++ myfile.py
@@ -1,6 +1,2 @@
-
-# f = open('myfile.txt', 'w', encoding="utf-8")
-
 with open('myfile.txt', 'w', encoding="utf-8") as f:
-    write_data = f.write('Hello World')
-    print(write_data)
+    f.write('Hello World')
